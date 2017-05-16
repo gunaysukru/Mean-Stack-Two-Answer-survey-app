b1b66f0b9194f3dd2b2cae15b65bb460d4df9464
DESKTOP-08FDMGO\GunaySukru <gunaysukru@gmail.com>
2017-05-16 11:52:44 +0300

    Angularjs http yerine https üzerinden çekiliyor.

--- app.js
+++ app.js
@@ -2,13 +2,3 @@
  * Created by GunaySukru on 15.05.2017.
  */
 var app = angular.module("surveyApp", ["ui.router", "angularModalService"]);
-
-app.factory('DisableSSL', function ($location, $window) {
-    return {
-        activate: function () {
-            if ($location.protocol() !== 'http') {
-                $window.location.href = $location.absUrl().replace('https', 'http');
-            }
-        }
-    };
-})
